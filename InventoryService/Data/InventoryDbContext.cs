f6f82bba79721114d5627fd27a60744e963808a8
Nancy <nghazou35@gmail.com>
2026-01-29 21:13:01 -0400

    feat: bases de codigo generadas por copilot

---
+++ InventoryService/Data/InventoryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using InventoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
+
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+    }
+}
---
+++ InventoryService/Services/InventoryServiceLogic.cs
@@ -0,0 +1,48 @@
+﻿using InventoryService.Data;
+using InventoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Services
+{
+    public class InventoryServiceLogic
+    {
+        private readonly InventoryDbContext _context;
+
+        public InventoryServiceLogic(InventoryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<InventoryItem>> GetAllItemsAsync()
+        {
+            return await _context.InventoryItems.ToListAsync();
+        }
+
+        public async Task<InventoryItem?> GetItemByIdAsync(int id)
+        {
+            return await _context.InventoryItems.FindAsync(id);
+        }
+
+        public async Task AddItemAsync(InventoryItem item)
+        {
+            _context.InventoryItems.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateItemAsync(InventoryItem item)
+        {
+            _context.InventoryItems.Update(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemAsync(int id)
+        {
+            var item = await _context.InventoryItems.FindAsync(id);
+            if (item != null)
+            {
+                _context.InventoryItems.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
